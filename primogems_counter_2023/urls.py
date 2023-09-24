90c4b4b9e84b520b3cf85e68475f9aaa19107a76
Ablesh1 <michal.ablewski19@gmail.com>
2023-09-24 21:03:54 +0200

    Change ' to "

--- primogems_counter_2023/urls.py
+++ primogems_counter_2023/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include("home.urls")),
-    path('primogems/', include("primogems.urls")),
-    path('admin/', admin.site.urls),
+    path("", include("home.urls")),
+    path("primogems/", include("primogems.urls")),
+    path("admin/", admin.site.urls),
 ]
